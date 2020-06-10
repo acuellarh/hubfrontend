75f553bce005952b17973dbbc904cb79c6e34bd9
Oblum1989 <oscar.blum@gmail.com>
2020-06-10 01:52:18 -0500

    add styled component in view login and sign_in

--- src/components/registatrion/LoginForm.js
+++ src/components/registatrion/LoginForm.js
@@ -1,58 +1,95 @@
 import React from "react";
+import {
+  ButtonForm,
+  InputForm,
+  TitleForm,
+  LinkForm,
+  ContainerForm,
+} from "./StyledForm";
 
 const LoginForm = () => (
-  <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-    <div class="max-w-md w-full">
+  <ContainerForm >
+    <div className="max-w-md w-full">
       <div>
-        <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
-          Ingresar a tu cuenta
-        </h2>
-        <p class="mt-2 text-center text-sm leading-5 text-gray-600">
+        <TitleForm>Ingresar a tu cuenta</TitleForm>
+        <p className="mt-2 text-center text-sm leading-5 text-gray-600">
           o &nbsp;
-          <a href="/sign_in" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+          <LinkForm href="/sign_in" className="transition ease-in-out duration-150">
             Crear cuenta
-          </a>
+          </LinkForm>
         </p>
       </div>
-      <form class="mt-8" action="#" method="POST">
+      <form className="mt-8" action="#" method="POST">
         <input type="hidden" name="remember" value="true" />
-        <div class="rounded-md shadow-sm">
+        <div className="rounded-md shadow-sm">
           <div>
-            <input aria-label="Email address" name="email" type="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5" placeholder="Email address" />
+            <InputForm
+              aria-label="Email address"
+              name="email"
+              type="email"
+              required
+              className="placeholder-gray-500"
+              placeholder="Email address"
+            />
           </div>
-          <div class="-mt-px">
-            <input aria-label="Password" name="password" type="password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5" placeholder="Password" />
+          <div className="-mt-px">
+            <InputForm
+              aria-label="Password"
+              name="password"
+              type="password"
+              required
+              className="placeholder-gray-500"
+              placeholder="Password"
+            />
           </div>
         </div>
 
-        <div class="mt-6 flex items-center justify-between">
-          <div class="flex items-center">
-            <input id="remember_me" type="checkbox" class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" />
-            <label for="remember_me" class="ml-2 block text-sm leading-5 text-gray-900">
+        <div className="mt-6 flex items-center justify-between">
+          <div className="flex items-center">
+            <input
+              id="remember_me"
+              type="checkbox"
+              className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+            />
+            <label
+              for="remember_me"
+              className="ml-2 block text-sm leading-5 text-gray-900"
+            >
               Recuerdame
             </label>
           </div>
 
-          <div class="text-sm leading-5">
-            <a href="/" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+          <div className="text-sm leading-5">
+            <LinkForm href="/" className="transition ease-in-out duration-150">
               Olvidaste tu contraseña?
-            </a>
+            </LinkForm>
           </div>
         </div>
 
-        <div class="mt-6">
-          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
-            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-              <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
-                <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+        <div className="mt-6">
+          <ButtonForm
+            type="submit"
+            className="group transition duration-150 ease-in-out"
+          >
+            <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+              <svg
+                className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                  clip-rule="evenodd"
+                />
               </svg>
             </span>
             Ingresar
-          </button>
+          </ButtonForm>
         </div>
       </form>
     </div>
-  </div>
+  </ContainerForm>
 );
 
 export default LoginForm;
--- src/components/registatrion/StyledForm.js
+++ src/components/registatrion/StyledForm.js
@@ -1,19 +1,18 @@
-import styled from "styled-components"
-import tw from "tailwind.macro"
+import styled from "styled-components";
+import tw from "tailwind.macro";
 
-const StyledForm = styled.main.attrs({
-  className: "flex flex-col h-screen justify-center items-center bg-gray-100",
-})`
-  & {
-    form {
-      ${tw`bg-white text-center rounded py-8 px-5 shadow max-w-xs`}
-    }
-    input {
-      ${tw`border-gray-300 mb-4 w-full border-solid border rounded py-2 px-4`}
-    }
-    button {
-      ${tw`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded`}
-    }
-  }
-`
-export default StyledForm
+export const ButtonForm = styled.button`
+  ${tw`mt-6 relative w-full flex justify-center py-4 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700`}
+`;
+export const InputForm = styled.input`
+  ${tw`mb-6 appearance-none rounded-none relative block w-full px-3 py-4 border border-gray-300 text-gray-900 rounded-b-md rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5`}
+`;
+export const TitleForm = styled.h2`
+  ${tw`mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900`}
+`;
+export const LinkForm = styled.a`
+  ${tw`font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline`}
+`;
+export const ContainerForm = styled.form`
+  ${tw`min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8`}
+`;
