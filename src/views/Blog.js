877ff058361d7d06a7a526d38beafc66704b9fca
Oblum1989 <oscar.blum@gmail.com>
2020-06-13 01:33:49 -0500

    used fetch

--- src/views/Blog.js
+++ src/views/Blog.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Blog = () => (
-  <div>Blog</div>
-);
+class Blog extends Component {
+  constructor(){
+    super();
+    this.state = { categories: [] }
+  }
+
+  componentDidMount(){
+    fetch('https://hubbackend.herokuapp.com/api/v1/categories')
+      .then(response => {
+        return response.json()
+      })
+      .then(response => {
+        console.log(response)
+        this.setState({ categories: response })
+      })
+  }
+  render(){
+    return(
+    <div>Blog{console.log(this.state.categories)}</div>
+    )
+  }
+  
+}
 
 export default Blog;
